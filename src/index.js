93a3033aa25c90d2a357f733980aecd3f17bd5f9
feden2906 <feden2906@gmail.com>
2021-10-14 19:55:35 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {Provider} from "react-redux";
-import {store} from "./redux";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
+import App from './App';
+import './index.css';
+import { store } from "./redux";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -17,4 +17,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
